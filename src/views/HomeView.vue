aa7488649f57b754010dd85e0c1728a8f018e0dc
Moniruzzaman Shanto <42086179+Shanto75@users.noreply.github.com>
2023-04-11 12:54:58 +0600

    Update HomeView.vue

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,12 +2,37 @@
 import { getPosts } from '../api.js';
 import { getPostsById } from '../api.js';
 import { getCategories } from "../api.js";
+import { reactive } from 'vue';
 
 export default {
+
+  setup() {
+    const state = reactive({
+      categories: [],
+      categoryPosts: {}
+    });
+
+    async function fetchData() {
+      state.categories = await getCategories();
+      console.log(state.categories);
+
+      await Promise.all(
+        state.categories.map(async (category) => {
+          state.categoryPosts[category.name] = await getPostsById(category.id);
+        })
+      );
+    }
+    fetchData();
+
+
+    return {
+      state
+    };
+  },
   data() {
     return {
       posts: [],
-      categoryPosts: {},
+      categoryPosts:[],
       categories: [],
       loading: true,
     };
@@ -15,19 +40,19 @@ export default {
   async mounted() {
     this.categories = await getCategories();
     this.categories.forEach( async (category) =>{
-      let item = {}
-      this.categoryPosts[category.name] = JSON.stringify( await getPostsById(category.id));
-      // this.categoryPosts.push(item);
+      this.categoryPosts[category.name] = await getPostsById(category.id);
     })
     
-    
     this.posts = await getPosts();
-    // console.log(JSON.stringify(this.categoryPosts));
-    Object.entries( this.categoryPosts).forEach(([key, value]) => {
-        console.log(`${key} ${value}`);
-    });
     this.loading = false;
-    // console.log(this.posts);
+
+    // console.log(this.categoryPosts);
+
+    // Object.entries( this.categoryPosts).forEach(([category, posts]) => {
+    //   posts.forEach((post)=>{
+    //       console.log(post.id);
+    //   })
+    // });
   },
 };
 </script>
@@ -81,42 +106,34 @@ export default {
               </h2>
             </div>
           </div>
-
         </div>
 
       </div>
     </section> <!-- End Post Grid Section -->
 
-    
-    <!-- ======= Post Grid Section ======= -->
-    <section id="posts" class="posts">
-      <div class="container" data-aos="fade-up">
-        <div class="section-header d-flex justify-content-between align-items-center mb-5">
-          <h2>Latest Blogs</h2>
-          <div><a href="category.html" class="more">See All Lifestyle</a></div>
-        </div>
-
-        <div v-if="loading" class="d-flex justify-content-center">
-          <div class="spinner-border" role="status">
-            <span class="visually-hidden">Loading...</span>
+    <section class="posts">
+      <div v-for="categoryPosts in state.categoryPosts" :key="categoryPosts" class="container" data-aos="fade-up">
+        <div v-if="categoryPosts.length > 0 ">
+          <div class="section-header d-flex justify-content-between align-items-center mb-5">
+            <h2>Latest Blogs</h2>
+            <div><router-link to="" class="more">See All</router-link></div>
           </div>
-        </div>
-
-        <div class="row row-cols-auto">
-          <div v-for="post in posts" :key="post.id" class="col-lg-3 col-md-6 ">
-            <div class="post-entry-1">
-              <router-link :to="'/blog/' + post.id"><img :src="post?._embedded?.['wp:featuredmedia']?.[0]?.source_url" alt="" class="img-fluid"></router-link>
-              <div class="post-meta"><span class="date">Sport</span> <span class="mx-1">&bullet;</span> <span>{{ post.date }}</span></div>
-              <h2>
-                <router-link :to="'/blog/' + post.id" >{{ post.title.rendered }}</router-link>
-              </h2>
+  
+          <div class="row row-cols-auto">
+            <div v-for="catPost in categoryPosts" :key="catPost.id" class="col-lg-3 col-md-6 ">
+              <div v-if="catPost" class="post-entry-1">
+                <router-link :to="'/blog/' + catPost.id"><img :src="catPost?._embedded?.['wp:featuredmedia']?.[0]?.source_url" alt="" class="img-fluid"></router-link>
+                <div class="post-meta"><span class="date">Sport</span> <span class="mx-1">&bullet;</span> <span>{{ catPost.date }}</span></div>
+                <h2>
+                  <router-link :to="'/blog/' + catPost.id" >{{ catPost.title.rendered }}</router-link>
+                </h2>
+              </div>
             </div>
           </div>
-
         </div>
 
       </div>
-    </section> <!-- End Post Grid Section -->
+    </section>
 
   </main><!-- End #main -->
 </template>
